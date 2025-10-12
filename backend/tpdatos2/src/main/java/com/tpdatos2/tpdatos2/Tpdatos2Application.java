5550cb67d771956d2397074f0970f3bff5713ad6
katerinacejas <katerinacejas3@gmail.com>
2025-10-11 21:30:12 -0300

    template backend y frontend

---
+++ backend/tpdatos2/src/main/java/com/tpdatos2/tpdatos2/Tpdatos2Application.java
@@ -0,0 +1,13 @@
+package com.tpdatos2.tpdatos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tpdatos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tpdatos2Application.class, args);
+	}
+
+}
---
+++ backend/tpdatos2/src/test/java/com/tpdatos2/tpdatos2/Tpdatos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpdatos2.tpdatos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tpdatos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
